459ccdd3a816d943090a3b637e6549114503241c
Andres Jaanson <A_Jaanson@hotmail.com>
2018-09-30 21:28:06 +0200

    Make an add microservice

---
+++ add-service/src/main/java/com/awscalculator/addservice/Response.java
@@ -0,0 +1,13 @@
+package com.awscalculator.addservice;
+
+public class Response {
+	private int value;
+	
+	public void setValue(int value) {
+		this.value = value;
+	}
+	
+	public int getValue() {
+		return this.value;
+	}
+}
