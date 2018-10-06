949c03e13680c481bf65b29b7880b4db999fa80f
Andres Jaanson <A_Jaanson@hotmail.com>
2018-10-06 20:46:09 +0200

    Create aws lambda function for each service

---
+++ services/multiply.js
@@ -0,0 +1,7 @@
+exports.multiplyHandler = function(event, context, callback) {
+   console.log("a = " + event.a);
+   console.log("b = " + event.b);
+   let answer = event.a * event.b;
+   console.log("answer = " + answer);
+   callback(null, answer);
+}
