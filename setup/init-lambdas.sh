ea6dde07444082fa19b6700e4d5fbba55299ed08
Ubuntu <ubuntu@ip-172-31-42-34.eu-central-1.compute.internal>
2018-10-07 22:04:18 +0000

    Make script publish add function

--- setup/init-lambdas.sh
+++ setup/init-lambdas.sh
@@ -5,7 +5,7 @@ if [ "$?" -ne "0" ]
 then
 	aws iam create-role --role-name $role_name --assume-role-policy-document file://iamtrustdocument.json
 fi
-role_arn=$(aws iam get-role --role-name $role_name --query Role.Arn)
+role_arn=$(aws iam get-role --role-name $role_name --query Role.Arn --output text)
 
 api_name="CalculatorLambdaAPI"
 
@@ -43,6 +43,61 @@ do
 		--runtime nodejs8.10 \
 		--role $role_arn \
 		--handler $endpoint.compute \
-		--zip-file fileb://igor.zip \
+		--zip-file fileb://../services/$endpoint.zip \
+		--region $region
+
+	lambda_arn=$(aws lambda list-functions \
+		--query "Functions[?FunctionName=='$endpoint'].FunctionArn" \
+		--output text --region $region)
+
+	aws apigateway put-integration \
+		--rest-api-id $api_id \
+		--resource-id $resource_id \
+		--http-method POST \
+		--type AWS \
+		--integration-http-method POST \
+		--uri arn:aws:apigateway:$region:lambda:path/2015-03-31/functions/$lambda_arn/invocations \
+		--request-templates '{"application/x-www-form-urlencoded":"{\"body\": $input.json(\"$\")}"}'
+		--region $region
+
+	aws apigateway put-method-response \
+		--rest-api-id $api_id \
+		--resource-id $resource_id \
+		--http-method POST \
+		--status-code 200 \
+		--response-models "{}" \
+		--region $region
+
+	aws apigateway put-integration-response \
+		--rest-api-id $api_id \
+		--resource-id $resource_id \
+		--http-method POST \
+		--status-code 200 \
+		--selection-pattern ".*" \
+		--region $region
+
+	api_arn=$(echo $lambda_arn | sed -e 's/lambda/execute-api/' -e "s/function:$endpoint/$api_id/")
+	aws lambda add-permission \
+		--function-name $endpoint \
+		--statement-id apigateway-$endpoint-test \
+		--action lambda:InvokeFunction \
+		--principal apigateway.amazonaws.com \
+		--source-arn "$api_arn/*/POST/$endpoint" \
+		--region $region
+
+	aws lambda add-permission \
+		--function-name $endpoint \
+		--statement-id apigateway-$endpoint-prod \
+		--action lambda:InvokeFunction \
+		--principal apigateway.amazonaws.com \
+		--source-arn "$api_arn/prod/POST/$endpoint" \
 		--region $region
 done
+
+aws apigateway create-deployment \
+	--rest-api-id $api_id \
+	--stage-name prod \
+	--region $region
+
+echo "The url you have to use in your Slack settings is:
+https://$api_id.execute-api.$region.amazonaws.com/prod/add"
